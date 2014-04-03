2c8b95ef70174c8fd1d106e8896f29a38e0e676a
akutsu_keita <akutsu_keita@cyberagent.co.jp>
2014-04-03 11:55:36 +0900

    テスト追加 statusコードのテスト

--- forum/test/test_app.js
+++ forum/test/test_app.js
@@ -1,5 +1,6 @@
 var request = require('supertest');
 var app = require('../app');
+var expect = require('expect.js');
 
 /**
  * '/'にアクセスすると'/top'にリダイレクトされる
@@ -10,6 +11,39 @@ describe('/', function() {
         var expected_body = 'Hello World';
         request(app)
             .get('/')
+            .expect(302)
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+            });
+    });
+});
+
+/**
+ * '/sessions/new'
+ */
+describe('/sessions/new', function() {
+    it('return 200 and get [title]', function() {
+        request(app)
+            .get('/sessions/new')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                //expect(res.param('title')).to.be('title');
+            });
+    });
+});
+
+/**
+ * /sessions/destroy　にアクセスしたら302
+ */
+describe('/sessions/destroy', function() {
+    it('return 302', function() {
+        request(app)
+            .get('/sessions/destroy')
             .expect(302)
             .end(function(err, res) {
                 if (err) {
